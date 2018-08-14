cfe8c7410ff12b3d5afb70e00e4d576ef9d97245
Tony Huang <tonyhuang@hotmail.co.uk>
2018-08-14 17:35:47 +0100

    Update: Managed to load the website bootstrap template. Added the navbar. Need to implement add accounts/orders.

---
+++ src/components/footer.vue
@@ -0,0 +1,41 @@
+<template>
+    <div id="footer">
+      <footer class="container-fluid text-center">
+        <p>Footer Text</p>
+      </footer>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'footer'
+}
+</script>
+
+<style scoped>
+  /* Remove the navbar's default margin-bottom and rounded borders */
+  .navbar {
+    margin-bottom: 0;
+    border-radius: 0;
+  }
+
+  /* Add a gray background color and some padding to the footer */
+  footer {
+    border: dashed red 2px;
+    background-color: #f2f2f2;
+    padding: 25px;
+  }
+
+  .carousel-inner img {
+    width: 100%; /* Set width to 100% */
+    margin: auto;
+    min-height:200px;
+  }
+
+  /* Hide the carousel text when the screen is less than 600 pixels wide */
+  @media (max-width: 600px) {
+    .carousel-caption {
+      display: none;
+    }
+  }
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,18 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import homepage from '../components/Homepage'
+// import navigation from '../components/Navigation'
 
+// import BootstrapVue from 'bootstrap-vue'
+//
+// Vue.use(BootstrapVue)
 Vue.use(Router)
-
 export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'homepage',
+      component: homepage
     }
   ]
 })
